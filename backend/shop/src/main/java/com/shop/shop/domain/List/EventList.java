291306628f42a8aecd8936fd276ed9cbaebeef8a
이경준 <kanell0304@gmail.com>
2025-03-25 14:12:59 +0900

    수정1

--- backend/shop/src/main/java/com/shop/shop/domain/List/EventList.java
+++ backend/shop/src/main/java/com/shop/shop/domain/List/EventList.java
@@ -1,4 +1,16 @@
 package com.shop.shop.domain.List;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventList {
+    
+    // 테스트
+    
 }
