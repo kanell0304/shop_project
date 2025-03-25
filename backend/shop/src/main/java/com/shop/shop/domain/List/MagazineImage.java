df59795c32603267bdfab714d4b92272e4dfff93
이경준 <kanell0304@gmail.com>
2025-03-25 15:46:16 +0900

    이벤트, 메거진, QnA 작업 추가

--- backend/shop/src/main/java/com/shop/shop/domain/List/MagazineImage.java
+++ backend/shop/src/main/java/com/shop/shop/domain/List/MagazineImage.java
@@ -1,4 +1,25 @@
 package com.shop.shop.domain.List;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MagazineImage {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "magazine_image_id")
+    private Long id;
+
+    private String fileName;
+
+    @Setter
+    private int ord;
+
+    @Column(name = "magazine_id")
+    private Long magazineId;
+
 }
