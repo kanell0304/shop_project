98f4ec9bcbe657f5c4b2023000c26176829a1539
강지석 <ever991122@gmail.com>
2025-04-16 14:46:56 +0900

    푸터 정책관련 설정  및 라우터 수정

---
+++ frontend/shop/src/Pages/footer/TermsPage.jsx
@@ -0,0 +1,16 @@
+import BasicLayout from "../../layout/BasicLayout";
+
+const TermsPage = () => {
+    return(
+        <BasicLayout>
+         <h2>이용약관</h2>
+         <div>
+
+
+
+         </div>
+        </BasicLayout>
+    )
+}
+
+export default TermsPage;
